5b8cb5da953534235508cd91f955c590ec98ded8
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-13 19:18:40 +0100

    repository add Cars

---
+++ Repository/CarDamagedRepository.cs
@@ -0,0 +1,52 @@
+﻿using CarTradeCenter.Contracts;
+using CarTradeCenter.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.Repository
+{
+    public class CarDamagedRepository : ICarDamagedRepository
+    {
+        public bool Create(CarDamaged entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(CarDamaged entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CarDamaged> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CarDamaged FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CarDamaged> GetAllVehiclesDamaged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CarDamaged> GetVehiclesDamagedByname(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CarDamaged entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/CarRepository.cs
@@ -0,0 +1,52 @@
+﻿using CarTradeCenter.Contracts;
+using CarTradeCenter.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public bool Create(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> GetAllVehiclesUndamaged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> GetVehiclesByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
