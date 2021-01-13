748203b15856129923b08bfac3fef67dd39b26ba
Marek Sowa <Marek.Sowa@ibm.com>
2021-01-13 19:04:01 +0100

    data models add Vehicle Image Car

---
+++ Data/CarDamaged.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.Data
+{
+    public class CarDamaged : Vehicle   
+    {
+        public string DamageDescription  { get; set; }
+    }
+}
---
+++ Data/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.Data
+{
+    public abstract class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime DateAuctionEnd { get; set; }
+        public DateTime DateAuctionStart { get; set; }
+        public Image Image { get; set; }
+    }
+}
