177d9cf141dc8bc3dbf086200f3bc02ac67c07c3
Marek Sowa <Marek.Sowa@ibm.com>
2021-05-11 18:08:44 +0200

    model User

---
+++ Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.Data.Models {
+    public class User 
+    {
+        public string Id { get; set;}
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public double Amount { get; set; }
+    }
+}
