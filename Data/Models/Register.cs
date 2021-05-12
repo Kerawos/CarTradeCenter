f68a7cfe4121ceeefd69acfdb3c29d02200b6481
Marek Sowa <Marek.Sowa@ibm.com>
2021-05-12 18:16:53 +0200

    create Register model with password

---
+++ Data/Models/Register.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CarTradeCenter.Data.Models
+{
+    public class Register
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdz Hasło")]
+        [Compare("Password", ErrorMessage ="Hasła nie zgadzają się ze sobą.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
