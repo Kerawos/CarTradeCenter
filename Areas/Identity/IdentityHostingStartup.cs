4b44f301f4e4d5bd89a33a5f8c4df757a8ed74ee
Marek Sowa <Marek.Sowa@ibm.com>
2021-05-13 18:00:35 +0200

    scaffold register, login, logout cshtml + contr

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CarTradeCenter.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarTradeCenter.Areas.Identity.IdentityHostingStartup))]
+namespace CarTradeCenter.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
