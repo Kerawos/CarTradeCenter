7db0fcb489263c4f919371aa744ca8285bd4263c
Marek Sowa <Marek.Sowa@ibm.com>
2021-06-30 22:07:49 +0200

    add BackgroundServices VehicleArchiver

---
+++ BackgroundServices/VehicleArchiver.cs
@@ -0,0 +1,40 @@
+﻿using CarTradeCenter.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarTradeCenter.BackgroundServices
+{
+    public class VehicleArchiver : BackgroundService, IHostedService
+    {
+        private readonly IRepositoryVehicle Repo;
+        private readonly int Time1h = 6000000;
+        private readonly int CarLimit = 9999;
+
+
+        public VehicleArchiver(IServiceScopeFactory factory)
+        {
+            this.Repo = factory.CreateScope().ServiceProvider.GetRequiredService<IRepositoryVehicle>();
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                TryToArchiveVehicles(CarLimit);
+                await Task.Delay(Time1h);
+            }
+        }
+
+
+        private void TryToArchiveVehicles(int vehiclesToArchive)
+        {
+
+        }
+    }
+}
