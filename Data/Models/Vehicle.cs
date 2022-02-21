76966b35fe99827de15fc5ec64f4dbb8573739db
Marek Sowa <Marek.Sowa@ibm.com>
2022-02-21 20:07:53 +0100

    db vehicle update

--- Data/Models/Vehicle.cs
+++ Data/Models/Vehicle.cs
@@ -27,6 +27,7 @@ namespace CarTradeCenter.Data.Models
         public string GasType { get; set; }
         public string Engine { get; set; }
         public string PriceBrandNew { get; set; }
+        public string PriceRepairCost { get; set; }
         public string InfoExtra { get; set; }
         public string InfoUsableParts { get; set; }
         public bool IsDamaged { get; set; }
---
+++ Migrations/20220221190710_VehiclePriceRepairCost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarTradeCenter.Migrations
+{
+    public partial class VehiclePriceRepairCost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PriceBrandNew",
+                table: "Vehicles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceBrandNew",
+                table: "Vehicles");
+        }
+    }
+}
